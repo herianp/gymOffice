417a4e9a672bb7566876f5517aa2a0fb6c96939f
herianp <peta.herian@gmail.com>
2021-11-04 12:44:09 +0100

    @GetMapping without /

--- src/main/java/com/example/youtube/controller/MainController.java
+++ src/main/java/com/example/youtube/controller/MainController.java
@@ -1,9 +1,13 @@
 package com.example.youtube.controller;
 
+import com.example.youtube.models.EmployeeAndMessageDTO;
 import com.example.youtube.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -18,6 +22,13 @@ public class MainController {
     public String mainPage(Model model){
         model.addAttribute("employeeList",employeeService.findAllEmployee());
         return "index";
+    }
+
+    @PostMapping("/employee")
+    public String createEmployee(@ModelAttribute EmployeeAndMessageDTO employeeAndMessageDTO){
+        employeeService.save(employeeAndMessageDTO);
+
+        return "redirect:/";
     }
 
 
