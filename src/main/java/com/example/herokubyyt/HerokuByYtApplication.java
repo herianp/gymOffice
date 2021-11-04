5519177e3d4a6032e3d2bb3b8f32b107690e4d56
herianp <peta.herian@gmail.com>
2021-11-04 09:34:17 +0100

    first create

---
+++ src/main/java/com/example/herokubyyt/HerokuByYtApplication.java
@@ -0,0 +1,13 @@
+package com.example.herokubyyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuByYtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuByYtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/herokubyyt/HerokuByYtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokubyyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuByYtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
